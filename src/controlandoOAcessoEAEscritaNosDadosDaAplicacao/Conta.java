93ae7ac6f3d62b2b04fdd79610592cf4a403b651
enzo-piazentin <enzopiazentin17@gmail.com>
2025-12-29 11:18:01 -0300

    add: Exercício 1 contaBancaria

--- src/controlandoOAcessoEAEscritaNosDadosDaAplicacao/Conta.java
+++ src/controlandoOAcessoEAEscritaNosDadosDaAplicacao/Conta.java
@@ -2,9 +2,22 @@ package controlandoOAcessoEAEscritaNosDadosDaAplicacao;
 
 public class Conta {
     private double saldo;
-    private int numero;
+    private int numeroConta;
+    public String titular;
 
-    public void deposita(double valor){
-        saldo = saldo + numero;
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta(){
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta){
+        this.numeroConta = numeroConta;
     }
 }
--- src/controlandoOAcessoEAEscritaNosDadosDaAplicacao/horaDapratica.java
+++ src/controlandoOAcessoEAEscritaNosDadosDaAplicacao/horaDapratica.java
@@ -1,4 +1,19 @@
 package controlandoOAcessoEAEscritaNosDadosDaAplicacao;
 
 public class horaDapratica {
+    public static void main(String[] args) {
+        Conta contaBancaria = new Conta();
+
+        contaBancaria.setNumeroConta(123);
+        contaBancaria.setSaldo(1000);
+        contaBancaria.titular = "Enzo";
+
+        System.out.println("Número da conta: " + contaBancaria.getNumeroConta());
+        System.out.println("Saldo: " + contaBancaria.getSaldo());
+        System.out.println("Titular: "+ contaBancaria.titular);
+
+        contaBancaria.setSaldo(1500);
+        System.out.println("Novo Saldo: "+ contaBancaria.getSaldo());
+
+    }
 }
